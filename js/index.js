409c191c420e4831ab22ca08ea39aab8ca28fc19
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-10-02 10:39:10 -0300

    fusion del shoppingCart.html y su css al index

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 console.log("JS funcionando")
 
-let email = document.getElementById("email")
-let desktopMenu = document.getElementById("desktopMenu")
+// VENTANA SING UP DESDE EL MAIL
+const email = document.getElementById("email")
+const desktopMenu = document.getElementById("desktopMenu")
 
 email.onclick=()=> {
     desktopMenu.classList.toggle("inactive")
@@ -14,3 +15,25 @@ const toggleMenu = document.getElementById("toggleMenu")
 toggleMenu.onclick =() => {
     mobileMenu.classList.toggle("inactive")
 }
+
+// SHOPPING CART - Que aparezca y desaparezca cuando clickeo el icono del carrito
+const shoppingCartIcon = document.getElementById("navBar-shoppingCart");
+const shoppingCart = document.getElementById("shoppingCart")
+
+shoppingCartIcon.onclick = () => {
+    shoppingCart.classList.toggle("inactive")
+    console.log("que desaparezca el cart")
+}
+
+
+
+
+
+
+
+// const navBar__shoppingCart = document.getElementById("navBar-shoppingCart")
+// const shoppingCart = document.getElementById("shoppingCart")
+
+// navBar__shoppingCart.onclick = () => {
+//     shoppingCart.classList.toggle("inactive");
+// }
