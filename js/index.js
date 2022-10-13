6249f72cecdfc3921bb579ebd07931b33550041d
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-10-12 22:27:43 -0300

    modificaciones varias en myAccount.html y su css, creacion del array productos, algoritmos para renderizar los productos en el index.html, agregar los productos seleccionados (con el logo de agregar al carrito) a la seccion de carrito, sumar el total de cada precio, FALTA PODER ELIMINAR UN PRODUCTO SELECCIONADO DEL CARRITO CON EL LOGO DE LA CRUZ

--- js/index.js
+++ js/index.js
@@ -1,39 +1,48 @@
 console.log("JS funcionando")
 
-// VENTANA SING UP DESDE EL MAIL
+// VENTANA SING UP DESDE EL MAIL. Si el shopping cart esta abierto, cerrarlo automaticamente.
 const email = document.getElementById("email")
 const desktopMenu = document.getElementById("desktopMenu")
+const isEmailClosed = email.classList.contains("inactive")
 
 email.onclick=()=> {
+    const isShoppingCartClosed = shoppingCart.classList.contains("inactive")
+    if(!isShoppingCartClosed){
+        shoppingCart.classList.add("inactive")
+    }
     desktopMenu.classList.toggle("inactive")
 }
+// como hacer para poner que si es menos de 730px de pantalla, que no aparezca???
 
-// MOBILE MENU - Que aparezca y desaparezca cuando toco el iciono de toggle menu.
+
+
+
+// MOBILE MENU - Que aparezca y desaparezca cuando toco el iciono de toggle menu. Si el shopping cart está abierto, se cierra automaticamente.
 const mobileMenu = document.getElementById("mobileMenu")
 const toggleMenu = document.getElementById("toggleMenu")
 
+
+
 toggleMenu.onclick =() => {
+    const isShoppingCartClosed = shoppingCart.classList.contains("inactive");
+
+    if (!isShoppingCartClosed){
+        shoppingCart.classList.add("inactive")
+    }
     mobileMenu.classList.toggle("inactive")
+
 }
 
-// SHOPPING CART - Que aparezca y desaparezca cuando clickeo el icono del carrito
+
+// SHOPPING CART - Que aparezca y desaparezca cuando clickeo el icono del carrito. Ademas, si el mobile menu está abierto, se cierra en el mismo instante.
 const shoppingCartIcon = document.getElementById("navBar-shoppingCart");
 const shoppingCart = document.getElementById("shoppingCart")
 
 shoppingCartIcon.onclick = () => {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive")
+    }
     shoppingCart.classList.toggle("inactive")
-    console.log("que desaparezca el cart")
 }
-
-
-
-
-
-
-
-// const navBar__shoppingCart = document.getElementById("navBar-shoppingCart")
-// const shoppingCart = document.getElementById("shoppingCart")
-
-// navBar__shoppingCart.onclick = () => {
-//     shoppingCart.classList.toggle("inactive");
-// }
--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,13 +1,204 @@
-// CREACIÓN DE OBJETOS
-class Producto {
-    constructor (img, nombre, precio){
-        this.img = img;
-        this.nombre = nombre;
-        this.precio= precio
+// array de objetos
+const productos = [
+    {
+        id: 1,
+        img: "./assets/img/armchair2.jpg",
+        nombre: "Green Armchair",
+        precio: 190,
+    },
+    {
+        id: 2,
+        img: "./assets/img/armchair.jpg",
+        nombre: "Gray Armchair",
+        precio: 180,
+    },
+    {
+        id: 3,
+        img: "./assets/img/armchair3.jpg",
+        nombre: "Pink Armchair",
+        precio: 120,
+    },
+    {
+        id: 4,
+        img: "./assets/img/audio.jpg",
+        nombre: "Gramophore",
+        precio: 140,
+    },
+    {
+        id: 5,
+        img: "./assets/img/bike.jpg",
+        nombre: "Bike",
+        precio: 120,
+    },
+    {
+        id: 6,
+        img: "./assets/img/lamp.jpg",
+        nombre: "Lamp",
+        precio: 120,
+    },
+    {
+        id: 7,
+        img: "./assets/img/oldArmchair.jpg",
+        nombre: "Old Armchair",
+        precio: 130,
+    },
+    {
+        id: 8,
+        img: "./assets/img/vintageWardrobe.jpg",
+        nombre: "Old War",
+        precio: 170
+    },
+    {
+        id: 9,
+        img: "./assets/img/armchair2.jpg",
+        nombre: "Green Armchair",
+        precio: 190
+    },
+    {
+        id: 10,
+        img: "./assets/img/armchair.jpg",
+        nombre: "Gray Armchair",
+        precio: 180
+    },
+    {
+        id: 11,
+        img: "./assets/img/armchair3.jpg",
+        nombre: "Pink Armchair",
+        precio: 120
+    },
+    {
+        id: 12,
+        img: "./assets/img/audio.jpg",
+        nombre: "Gramophore",
+        precio: 140
+    },
+    {
+        id: 13,
+        img: "./assets/img/bike.jpg",
+        nombre: "Bike",
+        precio: 120
+    },
+    {
+        id: 14,
+        img: "./assets/img/lamp.jpg",
+        nombre: "Lamp",
+        precio: 120
+    },
+    {
+        id: 15,
+        img: "./assets/img/oldArmchair.jpg",
+        nombre: "Old Armchair",
+        precio: 130
+    },
+    {
+        id: 16,
+        img: "./assets/img/vintageWardrobe.jpg",
+        nombre: "Old War",
+        precio: 170
     }
+    
+
+];
+const carrito = [];
+const total = [];
+
+
+// se renderiza cada objeto del array de productos dentro de la section con el id "cards-container"
+const cardsContainer = document.getElementById("cards-container");
+const renderizarProductos = () => {
+    productos.forEach((producto) => {
+        cardsContainer.innerHTML += `
+        <article class="card1-container" >
+                <img src="${producto.img}" alt="imagen producto">
+                <div class="card1-container__descript">
+                    <div class="card1-container__descript-info">
+                        <p>$${producto.precio}</p>
+                        <p>${producto.nombre}</p>
+                    </div>
+                    <figure>
+                        <img src="./assets/icons/bt_add_to_cart.svg" alt="add logo" id="botonAgregarCart${producto.id}">
+                    </figure>
+                </div>
+            </article>
+        `
+    });
 }
+renderizarProductos()
 
-const productos = [];
+// console.log(cardsContainer.innerHTML);
 
-const greenArmchair = new Producto (assets/img/armchair2.jpg, "Green Armchair", 190);
-const grayArmchair = new Producto (assets/img/armchair.jpg, "Gray Armchair", 180);
+// seleccionar producto apretando el icono de "botonAgregarCart" y agregarlo en "productosCarrito". TAMBIÉN ir sumando el valor de cada producto para al final ponerlo con inner.html en "sectionTotal"
+
+// const botonAgregarCart = document.getElementById(`botonAgregarCart${producto.id}`); NO FUNCIONA HACERLO AFUERA Y ADENTRO PONER "botonAgregarCart.onclick "
+
+const agregarAlCarrito = () => {
+    productos.forEach(producto => {
+        document.getElementById(`botonAgregarCart${producto.id}`).onclick = () => {
+            carrito.push(producto);
+            console.log("agregue un producto al carrito");
+            document.getElementById("productosCarrito").innerHTML += `
+            <article class="card" id="productoNombre${producto.id}">
+                        <figure >
+                            <img src="${producto.img}" alt="imagen producto">
+                            <p>${producto.nombre}</p>
+                        </figure>
+                        <div>
+                            <p>$${producto.precio}</p>
+                            <img src="../assets/icons/icon_close.png" alt="" id="eliminarProducto${producto.id}">
+                        </div>
+                    </article>
+                `
+
+                // ACA SUMA EL TOTAL
+            total.push(producto.precio);
+            console.log(total);
+
+            let sumaTotal = 0;
+            for (let i=0; i<total.length ; i++) {
+                sumaTotal += total[i];
+            }
+            
+            console.log("El total es: "+sumaTotal);
+            document.getElementById("sectionTotal").innerHTML = 
+            `<p>Total: </p>
+            <p id="totalCarrito">$${sumaTotal}</p>
+            `
+        }
+    });
+}
+agregarAlCarrito();
+
+
+
+
+// eliminar productos del carrito cuando se aprieta la imagen de la flecha
+
+// const eliminarProducto = () => {
+//     productos.forEach(producto => {
+//         document.getElementById(`eliminarProducto${producto.id}`).onclick = () => {
+//             // COMO ELIMINAR ESE PRODUCTO????????
+//         }
+        
+//     });
+// }
+// eliminarProducto()
+
+
+// 1 saber el indice del elemento con su id
+                // let indiceProductoEminar = productos.indexOf(`eliminarProducto${producto.id}`)
+
+// 2do uso el metodo splice = carrito.splice(indiceProductoEminar,1)
+
+// sumar el total y que aparezca en el p id="totalCarrito"
+
+// 1ro: tomar el precio de CADA producto (armar un array)
+// 2do: hacer una suma de todos los precios, guardarlo en una variable
+// 3ro: mostrar el valor de esa variable por p
+
+// const sumarTotal = () => {
+//     carrito.forEach(producto => {
+//         const total += carrito.push(producto.precio)
+//         console.log(total);
+//     });
+// }
+// sumarTotal();
